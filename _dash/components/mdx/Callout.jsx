09209e6869efaca34736de258a8cf0505f5fa230
etamity <etamity@gmail.com>
2019-10-24 03:29:25 +0100

    Update

---
+++ _dash/components/mdx/Callout.jsx
@@ -0,0 +1,13 @@
+export default class Callout extends React.Component {
+  render() {
+    return (
+      <div className="callout callout-info">
+        <small className="text-muted">New Clients</small>
+        <br />
+        <strong className="h4">
+          9999
+        </strong>
+      </div>
+    );
+  }
+}
